2acfe9e632f0fab8cd9b82f7d0e27b88000b4bcb
AstrodiR2 <astrodir2@gmail.com>
2026-05-08 15:16:59 +0300

    Fix items using official Fabric Wiki API

--- src/main/java/com/astrodir2/zombieapoc/item/BandageItem.java
+++ src/main/java/com/astrodir2/zombieapoc/item/BandageItem.java
@@ -6,31 +6,32 @@ import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.UseAction;
 import net.minecraft.world.World;
 
 public class BandageItem extends Item {
     public BandageItem(Settings settings) { super(settings); }
 
     @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        user.setCurrentHand(hand);
+        return TypedActionResult.consume(user.getStackInHand(hand));
+    }
+
+    @Override
     public ItemStack finishUsing(ItemStack stack, World world, LivingEntity user) {
         if (!world.isClient && user instanceof PlayerEntity player) {
             player.addStatusEffect(new StatusEffectInstance(StatusEffects.INSTANT_HEALTH, 1, 1));
-            stack.decrement(1);
         }
+        if (stack.getCount() > 0) stack.decrement(1);
         return stack;
     }
 
     @Override
-    public int getMaxUseTime(ItemStack stack, net.minecraft.world.World world) { return 32; }
+    public UseAction getUseAction(ItemStack stack) { return UseAction.EAT; }
 
     @Override
-    public net.minecraft.util.UseAction getUseAction(ItemStack stack) {
-        return net.minecraft.util.UseAction.EAT;
-    }
-
-    @Override
-    public net.minecraft.util.TypedActionResult<ItemStack> use(World world, PlayerEntity user, net.minecraft.util.Hand hand) {
-        user.setCurrentHand(hand);
-        return net.minecraft.util.TypedActionResult.consume(user.getStackInHand(hand));
-    }
+    public int getMaxUseTime(ItemStack stack) { return 32; }
 }
