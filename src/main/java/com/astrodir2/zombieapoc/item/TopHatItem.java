96d00d5490e1c5b6cfff34afb2bf7eab214e9539
AstrodiR2 <astrodir2@gmail.com>
2026-05-08 15:05:48 +0300

    Fix TopHatItem equipment type error

--- src/main/java/com/astrodir2/zombieapoc/item/TopHatItem.java
+++ src/main/java/com/astrodir2/zombieapoc/item/TopHatItem.java
@@ -13,32 +13,21 @@ public class TopHatItem extends Item {
         super(settings);
     }
 
-    // Кожен тік перевіряємо чи шляпа в руці гравця
     @Override
     public void inventoryTick(ItemStack stack, World world, net.minecraft.entity.Entity entity, int slot, boolean selected) {
         if (!world.isClient && entity instanceof PlayerEntity player) {
-            // Перевіряємо чи шляпа одягнута в слот голови
             ItemStack helmet = player.getInventory().getArmorStack(3);
             if (helmet.getItem() == this) {
-                // Speed I
                 if (!player.hasStatusEffect(StatusEffects.SPEED)) {
                     player.addStatusEffect(new StatusEffectInstance(StatusEffects.SPEED, 40, 0, false, false, true));
                 }
-                // Regeneration I
                 if (!player.hasStatusEffect(StatusEffects.REGENERATION)) {
                     player.addStatusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 40, 0, false, false, true));
                 }
-                // +4 серця (Max Health boost)
                 if (!player.hasStatusEffect(StatusEffects.HEALTH_BOOST)) {
                     player.addStatusEffect(new StatusEffectInstance(StatusEffects.HEALTH_BOOST, 40, 1, false, false, true));
                 }
             }
         }
-    }
-
-    // Дозволяємо одягати як шолом
-    @Override
-    public net.minecraft.item.equipment.EquipmentType getEquipmentType() {
-        return net.minecraft.item.equipment.EquipmentType.HELMET;
     }
 }
